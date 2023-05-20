d6abc40c9bb22c368a762df8b17b18accac92c0d
SErAphLi <seraphlivery@gmail.com>
2023-05-20 15:48:12 +0800

    fix: fix microlink api

--- parser.ts
+++ parser.ts
@@ -49,7 +49,7 @@ class JSONLinkParser extends Parser {
 class MicroLinkParser extends Parser {
 	constructor() {
 		super();
-		this.api = 'https://api.microlink.io?url={{{url}}}';
+		this.api = 'https://api.microlink.io?url={{{url}}}&palette=true&audio=true&video=true&iframe=true';
 	}
 	process(data: any): { title: string; image: string; description: string } {
 		const title = data.data.title || '';
