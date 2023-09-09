dffb36731d26588036fd52c960fdb81daf07f331
rcv4 <68054150+rcv4@users.noreply.github.com>
2023-09-09 21:01:42 +0200

    feat: improve local parser
    + enable requests over electron browser, so it doesnt get blocked by anti-crawling tools like incapsula
    + improve image selection

--- parser.ts
+++ parser.ts
@@ -1,6 +1,7 @@
 import { Notice } from 'obsidian';
 import Mustache from 'mustache';
 import { requestUrl } from 'obsidian';
+const electronPkg = require('electron');
 
 export abstract class Parser {
 	api: string;
@@ -76,20 +77,52 @@ class IframelyParser extends Parser {
 }
 
 class LocalParser extends Parser {
-	process(data: any): { title: string; image: string; description: string } {
-		throw new Error('Method not implemented.');
-	}
+    process(data: any): { title: string; image: string; description: string } {
+        let title = data.title || '';
+        const image = data.image || '';
+        let description: string = data.description || '';
 
-	getTitle(doc: Document, url: URL): string {
-		let element = doc.querySelector('head meta[property="og:title"]');
-		if (element instanceof HTMLMetaElement) {
-			return element.content;
+        description = description.replace(/\n/g, ' ');
+        title = title.replace(/\n/g, ' ');
+
+        return { title, image, description };
+    }
+
+    getTitle(doc: Document, url: URL): string {
+        let element = doc.querySelector('head meta[property="og:title"]');
+        if (element instanceof HTMLMetaElement) {
+            return element.content;
+        }
+        element = doc.querySelector('head title');
+        if (element) {
+            return element.textContent;
+        }
+        return url.hostname;
+    }
+
+	meetsCriteria(element: Element): boolean {
+		//If inline - display:none
+		if(/display:\s*none/.test(element.getAttribute('style'))){
+			return false;
 		}
-		element = doc.querySelector('head title');
-		if (element) {
-			return element.textContent;
+
+		//hide images in navigation bar/header
+		let contains_header = false;
+		element.classList.forEach((val) => {
+			if(val.toLowerCase().contains('header')) {
+				contains_header = true;
+			}
+		})
+		if (element.id.toLowerCase().contains('header') || contains_header){
+			return false;
 		}
-		return url.hostname;
+
+		//recurse until <html>
+		if(element.parentElement != null){
+			return this.meetsCriteria(element.parentElement);
+		}
+
+		return true;
 	}
 
 	getImage(doc: Document, url: URL): string {
@@ -97,31 +130,89 @@ class LocalParser extends Parser {
 		if (element instanceof HTMLMetaElement) {
 			return element.content;
 		}
-		element = doc.querySelector('body img');
-		if (element) {
-			// Get image from document and return the full URL
-			let attribute = element.getAttribute('src');
-			if (attribute) {
-				if (!attribute.startsWith('data:')) {
-					attribute = new URL(attribute, url).href;
+
+		let selectors = [
+			'div[itemtype$="://schema.org/Product"] noscript img',
+			'div[itemtype$="://schema.org/Product"] img',
+			'#main noscript img',
+			'#main img',
+			'main noscript img',
+			'main img',
+			'*[role="main"] img',
+			'body noscript img',
+			'body img',
+		]
+
+		for (const selector of selectors) {
+			let images = doc.querySelectorAll(selector)
+
+			for (let index = 0; index < images.length; index++) {
+				const element = images[index];
+				if(!this.meetsCriteria(element)){
+					continue;
 				}
-				return attribute;
+				let attribute = element.getAttribute('src');
+				// Get image from document and return the full URL
+				if (attribute) {
+					return (element as HTMLImageElement).src;
+				}
 			}
 		}
+
 		return '';
 	}
 
-	getDescription(doc: Document): string {
-		let element = doc.querySelector('head meta[property="og:description"]');
-		if (element instanceof HTMLMetaElement) {
-			return element.content;
-		}
-		element = doc.querySelector('head meta[name="description"]');
-		if (element instanceof HTMLMetaElement) {
-			return element.content;
-		}
-		return '';
-	}
+    getDescription(doc: Document): string {
+        let element = doc.querySelector('head meta[property="og:description"]');
+        if (element instanceof HTMLMetaElement) {
+            return element.content;
+        }
+        element = doc.querySelector('head meta[name="description"]');
+        if (element instanceof HTMLMetaElement) {
+            return element.content;
+        }
+        return '';
+    }
+
+
+    async getHtmlByRequest(url: string): Promise<string> {
+        const html = await requestUrl({ url: url }).then((site) => {
+            return site.text;
+        });
+		return html;
+    }
+
+    async getHtmlByElectron(url: string): Promise<string> {
+        const { remote } = electronPkg;
+        const { BrowserWindow } = remote;
+
+        try {
+            const window = new BrowserWindow({
+                width: 1366,
+                height: 768,
+                webPreferences: {
+                    nodeIntegration: false,
+					contextIsolation: true,
+					sandbox: true,
+                    images: false,
+                },
+                show: false,
+            });
+            window.webContents.setAudioMuted(true);
+
+            await new Promise<void>((resolve, reject) => {
+				window.webContents.on("did-finish-load", (e: any) => resolve(e));
+				window.webContents.on("did-fail-load", (e: any) => reject(e));
+				window.loadURL(url);
+			});
+
+            let doc = await window.webContents.executeJavaScript("document.documentElement.outerHTML;")
+            return doc
+
+        } catch (ex) {
+            return null;
+        }
+    }
 
 	async parse(url: string): Promise<{
 		title: string;
@@ -129,9 +220,8 @@ class LocalParser extends Parser {
 		description: string;
 		url: string;
 	}> {
-		const html = await requestUrl({ url: url }).then((site) => {
-			return site.text;
-		});
+		let html = await this.getHtmlByElectron(url) || await this.getHtmlByRequest(url);
+
 		let parser = new DOMParser();
 		const doc = parser.parseFromString(html, 'text/html');
 		// get base url from document
@@ -142,8 +232,8 @@ class LocalParser extends Parser {
 		let title = this.getTitle(doc, uRL);
 		let image = this.getImage(doc, uRL);
 		let description = this.getDescription(doc);
-		description = description.replace(/\n/g, ' ');
-		return { title, image, description, url };
+
+		return { ...this.process({ title, image, description }), url };
 	}
 }
 
